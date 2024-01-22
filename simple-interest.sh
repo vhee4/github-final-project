5a252decbbb335753c464b5ae1f677aacfbb874e
AFOGU  VIVIAN CHIDINMA <78314824+vhee4@users.noreply.github.com>
2024-01-22 09:21:46 +0000

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,18 +4,18 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # vhee4
    # Input:
    # p, principal amount
    # t, time period in years
    # r, annual rate of interest
    # Output:
    # simple interest = p*t*r
-   echo "Enter the principal:"
+   echo "Enter the principal:$20"
    read p
-   echo "Enter rate of interest per year:"
+   echo "Enter rate of interest per year:$100"
    read r
-   echo "Enter time period in years:"
+   echo "Enter time period in years:$2"
    read t
    s=`expr $p \* $t \* $r / 100`
    echo "The simple interest is: "
